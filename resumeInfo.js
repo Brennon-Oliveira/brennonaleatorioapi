e84db772b3c0adb3babc298d604af8b86293455b
Brennon-Oliveira <brennonoliveira20014@gmail.com>
2021-05-23 22:13:40 -0300

    2.0.1

--- resumeInfo.js
+++ resumeInfo.js
@@ -18,8 +18,14 @@ const resumeInfo = {
     qualifications:[
         'Curso Webmaster Front-End Completo (Danki Code, 2020 - Carga Horária: 80 horas)',
         'Curso Design de Aplicativos (Danki Code, 2021 - Carga Horária: 3 horas)',
+        'Curso de JavaScript (SoloLearn, 2020)',
+        'Curso Produtividade para Programadores (Danki Code, 2021 - Carga Horária: 3 horas)',
+        'Curso Agência Web de Sucesso (Danki Code, 2021 - Carga Horária: 6 horas)',
+        'Curso de HTML (SoloLearn, 2020)',
+        'Curso de CSS (SoloLearn, 2020)',
         'Conhecimento em React e React native',
         'Curso de PHP (SoloLearn, 2020)',
+        'Curso de Java (SoloLearn, 2020)',
         'Curso Git e contribuições para projetos Open Source (Udemy, 2021 - 3,5 horas)',
     ],
     additionalInfos:[
